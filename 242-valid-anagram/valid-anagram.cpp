715a10fd57db61d29ded7a629fd765661b7743cc
Nitin <149176665+NitinSheshma@users.noreply.github.com>
2025-09-26 19:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
-        vector<int> count(26,0);
 
-        for(char &ch : s){
-            count[ch-'a']++;
+        vector<int> count(26,0);
+        for(int i=0; i<s.size(); i++){
+            count[s[i] -'a']++;
+            count[t[i] - 'a']--;
         }
-        for(char &ch : t){
-            count[ch-'a']--;
+        for(int i=0; i<26; i++){
+            if(count[i] !=0){
+                return false;
+            }
         }
-        bool allZero = all_of(begin(count), end(count), [] (int element){
-        return element  ==0;
-   });
-    return allZero;
+        return true;
     }
 };
